fd5c1cf2c87e9f89327738c0a47b0343994d2304
Cale Bickler <bicklerc@sou.edu>
2014-04-20 10:55:55 -0700

    VB POS project

---
+++ AddItem.vb
@@ -0,0 +1,53 @@
+﻿Public Class AddItem
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If lstEntree.SelectedIndex() = -1 Then 
+        Else
+            intMenu(intCurrIndex) = (lstEntree.SelectedIndex())
+            intCurrIndex = (intCurrIndex + 1)
+            bolEntreeUpdate = True
+        End If
+
+        If lstApp.SelectedIndex() = -1 Then
+        Else
+            intMenu(intCurrIndex) = (lstApp.SelectedIndex() + intEntreeCount)
+            intCurrIndex = (intCurrIndex + 1)
+            bolAppUpdate = True
+        End If
+
+        If lstDessert.SelectedIndex() = -1 Then
+        Else
+            intMenu(intCurrIndex) = (lstDessert.SelectedIndex() + intEntreeCount + intAppitizerCount)
+            intCurrIndex = (intCurrIndex + 1)
+            bolDessertUpdate = True
+        End If
+
+        If (bolEntreeUpdate Or bolAppUpdate Or bolDessertUpdate) Then
+            Me.Close()
+        Else
+            MessageBox.Show("Please select a menu item or close the form")
+        End If
+    End Sub
+
+    Private Sub AddItem_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim intCount As Integer = 0
+        While (intCount < intEntreeCount)
+            lstEntree.Items.Add(getStrMenu(intCount))
+            intCount = intCount + 1
+        End While
+
+        While (intCount < (intEntreeCount + intAppitizerCount))
+            lstApp.Items.Add(getStrMenu(intCount))
+            intCount = intCount + 1
+        End While
+
+        While (intCount < (intEntreeCount + intAppitizerCount + intDessertCount))
+            lstDessert.Items.Add(getStrMenu(intCount))
+            intCount = intCount + 1
+        End While
+    End Sub
+End Class
---
+++ Receipt.vb
@@ -0,0 +1,29 @@
+﻿Public Class Receipt
+
+    Private Sub Receipt_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim intCounter As Integer = 0
+        lstReceipt.Items.Add("----------------------------Spaghetti's Restaurant----------------------------")
+        lstReceipt.Items.Add("")
+        While intCounter < intCurrIndex
+            lstReceipt.Items.Add(getStrMenu(intMenu(intCounter).ToString()))
+            intCounter = intCounter + 1
+        End While
+        lstReceipt.Items.Add("")
+        lstReceipt.Items.Add("--------------------------------------------------------------------------------------------")
+        lstReceipt.Items.Add("")
+        lstReceipt.Items.Add("Sub Total:                                                          " + dblSubPrice.ToString("c"))
+        lstReceipt.Items.Add("Tax:                                                                   " + dblTax.ToString("c"))
+        lstReceipt.Items.Add("Total:                                                                 " + dblTotal.ToString("c"))
+        lstReceipt.Items.Add("")
+        lstReceipt.Items.Add("--------------------------------------------------------------------------------------------")
+        lstReceipt.Items.Add("")
+        lstReceipt.Items.Add(strFirstName)
+        lstReceipt.Items.Add(strLastName)
+        lstReceipt.Items.Add(strAddress)
+        lstReceipt.Items.Add(strCity)
+        lstReceipt.Items.Add(strState)
+        lstReceipt.Items.Add(strZip)
+        lstReceipt.Items.Add(strPhone)
+        Me.Focus()
+    End Sub
+End Class
